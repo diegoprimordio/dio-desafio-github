05a45c1948226842be950e00c1b38233357e96d1
diegoprimordio <diego.mendes.piano@gmail.com>
2022-05-16 21:41:42 -0300

    Criando a pasta e os primeiros demos para o curso trabalhando-com-tipo-referenciados

---
+++ trabalhando-com-tipo-referenciados/Demos/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    public class Pessoa
+    {
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+
+    }
+}
---
+++ trabalhando-com-tipo-referenciados/Demos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Demos
+{
+    internal class Program
+    {
+        static void Demo1()
+        {
+            int a = 2;
+            a = Adicionar20(a);
+            Console.WriteLine($"O valor da variável a = {a}");
+        }
+
+        static int Adicionar20(int x)
+        {
+            return x + 20;
+        }
+
+        static void TrocarNome(Pessoa p1, string novoNome)
+        {
+            p1.Nome = novoNome;
+        }
+
+        static void Main()
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Diego Mendes";
+            p1.Idade = 37;
+            p1.Documento = "123123123";            
+
+            TrocarNome(p1, "José da silva");
+
+            Console.WriteLine($"Nome depois do método = {p1.Nome}");
+        }
+    }
+}
