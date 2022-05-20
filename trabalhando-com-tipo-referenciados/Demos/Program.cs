aaae4a0dabdeb864b930e4a6c1860ebbaa5d1e3c
diegoprimordio <diego.mendes.piano@gmail.com>
2022-05-19 21:57:06 -0300

    Sequencia dos estudos de reference types

--- trabalhando-com-tipo-referenciados/Demos/Program.cs
+++ trabalhando-com-tipo-referenciados/Demos/Program.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Demos
 {
     internal class Program
     {
+        static void Demo6()
+        {
+            int[] numeros = new int[] { 0, 2, 4, 6, 8 };
+            Console.WriteLine($"Digite o número que gostaria de encontar");
+            var numero = int.Parse(Console.ReadLine());
+
+            var idxEncontrado = EncontrarNumero(numeros, numero);
+
+            if (idxEncontrado >= 0)
+            {
+                Console.WriteLine($"O numero está na posição {idxEncontrado}");
+            }
+            else
+            {
+                Console.WriteLine($"O numero digitado não foi encontrado");
+            }
+
+        }
+
+        static void Demo5()
+        {
+            int[] pares = new int[] { 0, 2, 4, 6, 8 };
+            MudarParaImpar(pares);
+            Console.WriteLine($"O ímpares são {string.Join(",", pares)}");
+        }
+
+        static void Demo4()
+        {
+            string nome = "Diego";
+
+            TrocarNome(nome, "João");
+
+            Console.WriteLine($"O novo nome é {nome}");
+        }
+
         static void Demo3()
         {
             StructPessoa p1 = new StructPessoa()
@@ -66,13 +102,76 @@ namespace Demos
             nome = novoNome;
         }
 
+        static void MudarParaImpar(int[] pares)
+        {
+            for (int i = 0; i < pares.Length; i++)
+            {
+                pares[i] = pares[i] + 1;
+            }
+        }
+
+        static int EncontrarNumero(int[] numeros, int numero)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numero == numeros[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static bool EncontrarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+        {
+            foreach (var item in pessoas)
+            {
+                if (item.Nome == pessoa.Nome)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool EncontrarPessoa(List<StructPessoa> pessoas, StructPessoa pessoa)
+        {
+            foreach (var item in pessoas)
+            {
+                if (item.Equals(pessoa))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         static void Main()
         {
-            string nome = "Diego";
-            
-            TrocarNome(nome, "João");
+            List<StructPessoa> pessoas = new List<StructPessoa>()
+            {
+                new StructPessoa() {Nome = "Diego"},
+                new StructPessoa() {Nome = "Izabelle"},
+                new StructPessoa() {Nome = "João"},
+                new StructPessoa() {Nome = "Antonio"},
+                new StructPessoa() {Nome = "Ernesto"}
+            };
 
-            Console.WriteLine($"O novo nome é {nome}");
+            Console.WriteLine("Digite a pessoa que gostaria de localizar");
+            var nome = Console.ReadLine();
+            var pessoa = new StructPessoa() { Nome = nome};
+            var encontrado = EncontrarPessoa(pessoas, pessoa);
+
+            if (encontrado)
+            {
+                Console.WriteLine("A pessoa foi encontrada.");
+            }
+            else
+            {
+                Console.WriteLine("A pessoa não foi encontada.");
+            }
+
+
         }
     }
 }
