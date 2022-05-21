9745e8e0b34268e3c51d633673e21ba500b6f952
diegoprimordio <diego.mendes.piano@gmail.com>
2022-05-21 13:48:33 -0300

    Subindo arquivos da aula 4

---
+++ trabalhando-com-tipo-referenciados/Aula4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula4
+{
+    internal class Program
+    {
+        class Numero
+        {
+            public int N { get; set; }
+            public Numero(int n)
+            {
+                N = n;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Numero a = new Numero(2);
+            Numero b = new Numero(2);
+
+            if (a.N == b.N)
+            {
+                Console.WriteLine("a e b são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("a e b não são iguais");
+            }
+        }
+    }
+}
