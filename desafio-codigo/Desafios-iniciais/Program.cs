a9555cd2f7b87c5545f5a2772f918bf3a440ce77
diegoprimordio <diego.mendes.piano@gmail.com>
2022-05-21 18:03:56 -0300

    Atualizando arquivos

---
+++ desafio-codigo/Desafios-iniciais/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Desafios_iniciais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int maior = 0;
+            int posicao = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > maior)
+                {
+                    maior =           ;
+                    posicao =       ;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
